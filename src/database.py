5a523e218e41745fd9d27455abc55a1944399e68
mohamed-ashour <e.mohamed77@gmail.com>
2016-09-16 14:20:01 +0200

    initialized db connection before request

---
+++ src/database.py
@@ -0,0 +1,12 @@
+from mongoframes import Frame
+
+from pymongo import MongoClient
+
+from src import app
+
+
+
+@app.before_first_request
+def initialize_db():
+    URI = app.config['DATABASE_URI']
+    Frame._client = MongoClient(URI)
